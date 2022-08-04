708c3030304fe0435d1fa34a0cb071f3d4344b06
praTeek271 <prateekpatriotasme@gmail.com>
2022-08-04 07:07:19 +0530

    whatsapp-integrated

--- code_buster/settings.py
+++ code_buster/settings.py
@@ -12,10 +12,12 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-
+from .info import *
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+ID=Account_SID
+TOKEN=AUTH_TOKEN
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
